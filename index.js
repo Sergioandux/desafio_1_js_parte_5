a5c7a045a4422fc77ee9cd30a54fcccbc3f2f2c8
sergioandux <sergioandux@gmail.com>
2021-11-25 16:14:30 -0300

    parte 5 lista

---
+++ index.js
@@ -0,0 +1,8 @@
+var numero1 = parseFloat(prompt("ingrese primer número"));
+var numero2 = parseFloat(prompt("ingrese segundo número"));
+var numero3 = parseFloat(prompt("ingrese tercer número"));
+var numero4 = parseFloat(prompt("ingrese cuarto número"));
+var numero5 = parseFloat(prompt("ingrese quinto número"));
+var suma = numero1+numero2+numero3+numero4+numero5;
+document.write("la suma de los números ingresados es: "+suma+"<br>");
+document.write("el promedio de los números ingresados es: "+suma/5);
